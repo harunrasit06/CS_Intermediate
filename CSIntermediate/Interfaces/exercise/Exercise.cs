01e6efeb1c1eb6c5f34bc2664394c5c02e3ebf17
Harun Rasit Aktas <harunrasitaktas@gmail.com>
2024-02-20 09:57:35 +0100

    Interface Exercise

--- CSIntermediate/Interfaces/exercise/Exercise.cs
+++ CSIntermediate/Interfaces/exercise/Exercise.cs
@@ -6,7 +6,65 @@ using System.Threading.Tasks;
 
 namespace CSIntermediate.Interfaces.exercise
 {
-    internal class Exercise
+    public interface IActivity
     {
+        void Execute();
     }
-}
+
+    public class WorkflowEngine
+    {
+        private readonly List<IActivity> _workflow;
+        public WorkflowEngine(List<IActivity> workflow)
+        {
+            _workflow = workflow;
+        }
+        public void Run()
+        {
+            foreach (IActivity activity in _workflow)
+            {
+                activity.Execute();
+            }
+        }
+    }
+
+    public class Upload : IActivity
+    {
+        public void Execute()
+        {
+            System.Console.WriteLine("Uploading video to the cloud storage...");
+        }
+    }
+    public class Call : IActivity
+    {
+        public void Execute()
+        {
+            System.Console.WriteLine("Calling the web service provided by a third-party video encoding service to tell them thez have a video ready for encoding...");
+        }
+    }
+    public class SendEmail : IActivity
+    {
+        public void Execute()
+        {
+            System.Console.WriteLine("Sending email to the owner of the video notifying them that the video started processing... ");
+        }
+    }
+
+    public class ProcessingStatus : IActivity
+    {
+        public void Execute()
+        {
+            System.Console.WriteLine("Changing the status of the video record in the database to Processing...");
+        }
+    }
+    class Exercise
+    {
+        static void Main(string[] args)
+        {
+            var workflow = new List<IActivity> { new Upload(), new Call(), new SendEmail(), new ProcessingStatus() };
+
+            var workflowEngine = new WorkflowEngine(workflow);
+
+            workflowEngine.Run();
+        }
+    }
+    }
